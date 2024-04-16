8dd59e2c2fd630be6b70d3130a15ade07f94e566
Sami Gilliand <sami.gilliand@heig-vd.ch>
2024-04-16 15:33:34 +0200

    Modifications internes importantes - InputTemperature & code essentielles

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,25 @@
 <script setup>
   import AppTemperature from './components/AppTemperature.vue';
+  import inputTemperature from './components/inputTemperature.vue';
   import { computed, ref, watch } from 'vue';
-
-  const arrondir = (valeur) => {
-    const valeur2 = parseFloat(valeur).toFixed(2);
-    return parseFloat(valeur2);
-  }
 
   const tempSI = defineModel('tempSI', {type: Number, default: 273.15});
 
   const tempCel = defineModel('tempCel', {
     type : Number, 
     default: 0,
-    get : () => arrondir(tempSI.value - 273.15),
+    get : () => tempSI.value - 273.15,
     set : (newValue) => {
-      tempSI.value = arrondir(newValue + 273.15);
+      tempSI.value = newValue + 273.15;
     }
   });
 
   const tempFar = defineModel('tempFar', {
     type : Number,
     default : 0,
-    get : () => arrondir(tempSI.value * 9/5 - 459.67),
+    get : () => tempSI.value * 9/5 - 459.67,
     set : (newValue) => {
-      tempSI.value =  arrondir((newValue + 459.67) * 5/9);
+      tempSI.value =  (newValue + 459.67) * 5/9;
     }
   });
 
@@ -41,11 +37,11 @@
   //  }
   // });
 
-  watch(tempSI, (newVal) => {
-    console.log(
-      `Kelvin: ${newVal}, Celsius: ${tempCel.value}, Fahrenheit: ${tempFar.value}`
-    )
-  });
+  // watch(tempSI, (newVal) => {
+  //  console.log(
+  //    `Kelvin: ${newVal}, Celsius: ${tempCel.value}, Fahrenheit: ${tempFar.value}`
+  // )
+  // });
 
  // setInterval(() => {
  //   tempCel.value = Math.random() * 100;
@@ -56,7 +52,15 @@
 <template>
   <h2>Temperature Converter</h2>
 
-  <p class="title-temp">Kelvin</p>
+  <inputTemperature unit="Kelvin" v-model="tempSI" step="0.1"/>
+  <inputTemperature unit="Celsius" v-model="tempCel" step="0.1"/>
+  <inputTemperature unit="Farenheit" v-model="tempFar" step="0.1"/>
+
+  <p v-if="tempSI < 0">
+    Votre degré Kelvin est en dessous de 0... Are you sure about that ? 
+  </p>
+
+  <!-- p class="title-temp">Kelvin</p>
   <input type="number" v-model="tempSI" /> 
   <br>
 
@@ -72,7 +76,7 @@
 
   <p v-if="tempSI < 0">
     Votre degré Kelvin est en dessous de 0... Are you sure about that ? 
-  </p>
+  </p -->
 
   <!-- AppTemperature :temperature="tempSI" unit="°K"/>
   <AppTemperature :temperature="tempCel" unit="°C" />
--- src/components/inputTemperature.vue
+++ src/components/inputTemperature.vue
@@ -1,14 +1,35 @@
 <script setup>
-    defineProps({
-        name : String, 
-        modelValue : Number, 
+  const arrondir = (valeur) => {
+    const valeur2 = parseFloat(valeur).toFixed(2);
+    return parseFloat(valeur2);
+  } 
+
+    const props = defineProps({
+        unit : {
+          type: [Boolean, String],
+          required: false,
+        },
+        decimal: {
+          type: Number,
+          required: false,
+          default: 2,
+        }
+    });
+
+    defineOptions({
+      inheritAttrs : false
+    });
+
+    const model = defineModel({
+      get: () => {
+        return arrondir(props.modelValue);
+      }, 
     });
 </script>
 
 <template>
-  <p class="title-temp">{{ name }}</p>
-  <input type="number" v-model="modelValue" /> 
-  <br>
+    <p class="title-temp"> {{ unit }}</p>
+    <input type="number" v-model="model" v-bind="$attrs" /> 
 </template>
 
 <style scoped>
